5a903e12c7cd604de85951e871dc386b9b9326f3
Harry Fitzgerald <hjfitz@pm.me>
2022-05-06 22:26:57 +0100

    nice

---
+++ src/components/Window.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let title: string
+</script>
+
+<template>
+
+	<div class="window">
+			<div class="title-bar">
+				<div class="title-bar-text">{title}</div>
+				<div class="title-bar-controls">
+					<button aria-label="Minimize"></button>
+					<button aria-label="Maximize"></button>
+					<button aria-label="Close"></button>
+				</div>
+			</div>
+			<div class="window-body">
+				<slot></slot>
+			</div>
+		</div>
+
+</template>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,22 @@
 <template>
 	<section class="haiku-input" style="width: 300px">
-		<div class="window">
-			<div class="title-bar">
-				<div class="title-bar-text">Haiku Validator</div>
-				<div class="title-bar-controls">
-					<button aria-label="Minimize"></button>
-					<button aria-label="Maximize"></button>
-					<button aria-label="Close"></button>
-				</div>
+		<Window title="Haiku Validator">
+			<div class="field-row-stacked" style="width: 100%">
+				<label for="text20">Enter your haiku</label>
+				<textarea bind:value={haiku} rows="8" cols={30}/>
 			</div>
-			<div class="window-body">
-				<div class="field-row-stacked" style="width: 100%">
-					<label for="text20">Enter your haiku</label>
-					<textarea bind:value={haiku} rows="8" cols={30}/>
-				</div>
-			</div>
-		</div>
-	</section>
+		</Window>
+		</section>
 
-	<section class="haiku-results">
-		<div class="window" style="width: 300px">
-			<div class="title-bar">
-				<div class="title-bar-text">Haiku Validation Results</div>
-				<div class="title-bar-controls">
-					<button aria-label="Minimize"></button>
-					<button aria-label="Maximize"></button>
-					<button aria-label="Close"></button>
-				</div>
-			</div>
-			<div class="window-body">
+	<section class="haiku-results" style="width: 300px">
+		<Window title="Validation Results">
 				{#if displayResults}
 					<ol>
-						{#each lines as line, idx}
-							<li>{line.text} - valid: {line.valid}</li>
+						{#each lines as line}
+							<li>
+								<p>Evaluation: {line.text} - valid: {line.valid}</p>
+								<p>Syllables: {line.syllableCount.join(', ')}</p>
+							</li>
 						{/each}
 					</ol>
 				{:else}
@@ -40,22 +24,19 @@
 						<p>Enter your haiku to continue</p>
 					</div>
 				{/if}
-			</div>
 			<div class="status-bar">
 				<p class="status-bar-field">Press F1 for help</p>
-				{#if displayResults}
-					<p class="status-bar-field">Haiku valid: {isValidHaiku}</p>
-				{:else}
-					<p class="status-bar-field">Slide 1</p>
-				{/if}
+				<p class="status-bar-field">Haiku valid: {isValidHaiku}</p>
 				<p class="status-bar-field">CPU Usage: 14%</p>
 			</div>
-		</div>
+		</Window>
 
 	</section>
 </template>
 
 <script lang="ts">
+	import Window from '../components/Window.svelte'
+	import {from} from 'rxjs'
 	// todo: add types
 	import pronouncing from 'pronouncing/build/pronouncing-browser.js'
 
@@ -63,6 +44,7 @@
 		text: string
 		valid: boolean
 		syllables: number 
+		syllableCount: number[]
 	}
 
 	function haikuLineFactory(syllables: number = 5): HaikuLine {
@@ -70,28 +52,28 @@
 			text: '',
 			valid: false,
 			syllables,
+			syllableCount: [],
 		}
 	}
 
+	// textarea input
 	let haiku = '';
 
-	let line1 = haikuLineFactory()
-	let line2 = haikuLineFactory(7)
-	let line3 = haikuLineFactory()
-
-	let lines = [line1, line2, line3]
+	let lines = [haikuLineFactory(), haikuLineFactory(7), haikuLineFactory()]
 
 	let displayResults = false
 	let isValidHaiku = false
 
-	function syllableCount(line: string): number {
+	function getSyllableCountForWord(word: string): number {
+		return pronouncing.syllableCount(
+			pronouncing.phonesForWord(word)[0]
+		)
+	}
+
+	function syllableCount(line: string): number[] {
 		return line.split(' ')
 			.filter(Boolean)
-			.map(word => 
-				 pronouncing.syllableCount(
-					pronouncing.phonesForWord(word)[0]
-				)
-			).reduce((acc, cur) => acc + cur, 0)
+			.map(getSyllableCountForWord)
 	}
 
 
@@ -102,7 +84,9 @@
 			.forEach((line, idx) => lines[idx].text = line)
 
 		lines.forEach(line => {
-			line.valid = syllableCount(line.text) === line.syllables
+			const syllables = syllableCount(line.text)
+			line.valid = syllables.length === line.syllables
+			line.syllableCount = syllables
 		})
 
 		lines = lines
